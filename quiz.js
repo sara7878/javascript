0bad1b781a96f70d040ca21d438601ff33b4d560
mariaaemiil3 <mariaemil55@gmail.com>
2022-01-14 20:47:39 +0200

    Added timer

--- quiz.js
+++ quiz.js
@@ -66,14 +66,56 @@ const Quiz = {
   ],
 };
 
+var sec         = 1800,
+    countDiv    = document.getElementById("timer"),
+    secpass;
+    
+
+function secpass() {
+    'use strict';
+    
+    var min     = Math.floor(sec / 60),
+        remSec  = sec % 60;
+    
+    if (remSec < 10) {
+        
+        remSec = '0' + remSec;
+    
+    }
+    if (min < 10) {
+        
+        min = '0' + min;
+    
+    }
+    countDiv.innerHTML = min + ":" + remSec;
+    
+    if (sec > 0) {
+        
+        sec = sec - 1;
+        
+    } else {
+        
+        clearInterval(countDown);
+        
+        countDiv.innerHTML = 'countdown done';
+        
+    }
+}
+
 const startBtn = document.getElementById("startBtn");
 const nextBtn = document.getElementById("btn");
 
 startBtn.addEventListener("click", ()=>{
+    countDown   = setInterval(function () {
+        'use strict';
+        
+        secpass();
+    }, 1000);
     displayQuestion(1);
     startBtn.style.display="none";
     nextBtn.setAttribute("value","1"); 
     nextBtn.style.display="block";
+    
 });
 
 nextBtn.addEventListener("click", ()=>{
