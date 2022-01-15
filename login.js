0ae687e5b2b76fba22bc919935ed64113b25893f
mahm0ud-abdelhalim <mahmoud.abdelhalim.mostafa@outlook.com>
2022-01-15 14:17:36 +0200

    testing import

---
+++ login.js
@@ -0,0 +1,5 @@
+import { regUsers } from "./regestration.js";
+
+const loginUser = regUsers[1]
+
+export {loginUser}
---
+++ regestration.js
@@ -0,0 +1,12 @@
+const regUsers = [
+    {
+        username:"A",
+        gender:"male"
+    },
+    {
+        username:"B",
+        gender:"female"
+    },
+]
+
+export {regUsers}
